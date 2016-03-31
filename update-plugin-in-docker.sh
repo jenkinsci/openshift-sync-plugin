bff999ef1e0d16577da56d6035febb09692e1082
James Strachan <james.strachan@gmail.com>
2016-03-31 18:07:33 +0100

    really fix licence checks

--- update-plugin-in-docker.sh
+++ update-plugin-in-docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#
 # Copyright (C) 2016 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 export JENKINS_HOST="http://jenkins.vagrant.f8/"
 export JENKINS_CONTAINER=`docker ps | grep jenkins | { read first rest ; echo $first ; }`
