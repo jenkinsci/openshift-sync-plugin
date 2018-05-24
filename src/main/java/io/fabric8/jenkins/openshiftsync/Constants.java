e5f02a448bca6692fcd26348111506f9eb94f1fd
Gabe Montero <gmontero@redhat.com>
2018-05-24 13:51:29 -0400

    Fixed the issue of duplicate pipeline (#239)
    * Fixed the issue of duplicate pipeline
    
    Right now, when the buildconfig name is different then git repo name
    then sync plugin is not able to find buildconfig by repo name while
    syncing back to openshift after creating a buildconfig with strategy
    JenkinsPipeline, and create a new buildconfig repo name, To fix this
    issue, added a label of gitRepository name so it will first try to
    find the BuildConfig with name of jenkins job, if that is not present
    that is our case then it will also try to find the buildconfig having
    label with the name of jenkins job and then if it is not present will
    create a new BuildConfig #27
    
    Declared constant variable for label key
    
    Removed NPE and added comments in the code to always know the context behind this
    
    * Updated wrong logging in #28 PR

--- src/main/java/io/fabric8/jenkins/openshiftsync/Constants.java
+++ src/main/java/io/fabric8/jenkins/openshiftsync/Constants.java
@@ -19,37 +19,38 @@ package io.fabric8.jenkins.openshiftsync;
 /**
  */
 public class Constants {
-	public static final String OPENSHIFT_DEFAULT_NAMESPACE = "default";
+    public static final String OPENSHIFT_DEFAULT_NAMESPACE = "default";
 
-	public static final String OPENSHIFT_ANNOTATIONS_BUILD_NUMBER = "openshift.io/build.number";
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_BUILD_URI = "openshift.io/jenkins-build-uri";
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_LOG_URL = "openshift.io/jenkins-log-url";
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_CONSOLE_LOG_URL = "openshift.io/jenkins-console-log-url";
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_BLUEOCEAN_LOG_URL = "openshift.io/jenkins-blueocean-log-url";
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_PENDING_INPUT_ACTION_JSON = "openshift.io/jenkins-pending-input-actions-json";
+    public static final String OPENSHIFT_ANNOTATIONS_BUILD_NUMBER = "openshift.io/build.number";
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_BUILD_URI = "openshift.io/jenkins-build-uri";
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_LOG_URL = "openshift.io/jenkins-log-url";
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_CONSOLE_LOG_URL = "openshift.io/jenkins-console-log-url";
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_BLUEOCEAN_LOG_URL = "openshift.io/jenkins-blueocean-log-url";
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_PENDING_INPUT_ACTION_JSON = "openshift.io/jenkins-pending-input-actions-json";
 
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_STATUS_JSON = "openshift.io/jenkins-status-json";
-	public static final String OPENSHIFT_ANNOTATIONS_JENKINS_NAMESPACE = "openshift.io/jenkins-namespace";
-	public static final String OPENSHIFT_LABELS_BUILD_CONFIG_NAME = "openshift.io/build-config.name";
-	// see PR https://github.com/openshift/jenkins-sync-plugin/pull/189, there was a issue with having "/"
-	// in a label we construct a watch over, where usual UTF-8 encoding of the label name (which becomes part of 
-	// a query param on the REST invocation) was causing okhttp3 to complain (there is even more history/discussion
-	// in the PR as to issues with fixing).
-	// so we avoid use of "/" for this label
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_STATUS_JSON = "openshift.io/jenkins-status-json";
+    public static final String OPENSHIFT_ANNOTATIONS_JENKINS_NAMESPACE = "openshift.io/jenkins-namespace";
+    public static final String OPENSHIFT_LABELS_BUILD_CONFIG_NAME = "openshift.io/build-config.name";
+    public static final String OPENSHIFT_LABELS_BUILD_CONFIG_GIT_REPOSITORY_NAME = "openshift.io/gitRepository";
+    // see PR https://github.com/openshift/jenkins-sync-plugin/pull/189, there was a issue with having "/"
+    // in a label we construct a watch over, where usual UTF-8 encoding of the label name (which becomes part of 
+    // a query param on the REST invocation) was causing okhttp3 to complain (there is even more history/discussion
+    // in the PR as to issues with fixing).
+    // so we avoid use of "/" for this label
     public static final String OPENSHIFT_LABELS_SECRET_CREDENTIAL_SYNC = "credential.sync.jenkins.openshift.io";
     public static final String VALUE_SECRET_SYNC = "true";
 
-	public static final String OPENSHIFT_SECRETS_DATA_USERNAME = "username";
-	public static final String OPENSHIFT_SECRETS_DATA_PASSWORD = "password";
-	public static final String OPENSHIFT_SECRETS_DATA_SSHPRIVATEKEY = "ssh-privatekey";
-	public static final String OPENSHIFT_SECRETS_DATA_FILENAME = "filename";
-	public static final String OPENSHIFT_SECRETS_DATA_CERTIFICATE = "certificate";
-	public static final String OPENSHIFT_SECRETS_TYPE_SSH = "kubernetes.io/ssh-auth";
-	public static final String OPENSHIFT_SECRETS_TYPE_BASICAUTH = "kubernetes.io/basic-auth";
-	public static final String OPENSHIFT_SECRETS_TYPE_OPAQUE = "Opaque";
-	public static final String OPENSHIFT_BUILD_STATUS_FIELD = "status";
+    public static final String OPENSHIFT_SECRETS_DATA_USERNAME = "username";
+    public static final String OPENSHIFT_SECRETS_DATA_PASSWORD = "password";
+    public static final String OPENSHIFT_SECRETS_DATA_SSHPRIVATEKEY = "ssh-privatekey";
+    public static final String OPENSHIFT_SECRETS_DATA_FILENAME = "filename";
+    public static final String OPENSHIFT_SECRETS_DATA_CERTIFICATE = "certificate";
+    public static final String OPENSHIFT_SECRETS_TYPE_SSH = "kubernetes.io/ssh-auth";
+    public static final String OPENSHIFT_SECRETS_TYPE_BASICAUTH = "kubernetes.io/basic-auth";
+    public static final String OPENSHIFT_SECRETS_TYPE_OPAQUE = "Opaque";
+    public static final String OPENSHIFT_BUILD_STATUS_FIELD = "status";
 	
-	public static final String OPENSHIFT_PROJECT_ENV_VAR_NAME = "PROJECT_NAME";
+    public static final String OPENSHIFT_PROJECT_ENV_VAR_NAME = "PROJECT_NAME";
     public static final String OPENSHIFT_PROJECT_FILE = "/run/secrets/kubernetes.io/serviceaccount/namespace";
 
 
