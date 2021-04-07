d1652898a9793b7b1da98a175d73c003fc9403ca
Akram Ben Aissi <akram.benaissi@gmail.com>
2021-04-07 03:38:10 +0200

    Clean up code from BasePluginConfiguration

--- src/main/java/io/fabric8/jenkins/openshiftsync/BaseWatcher.java
+++ src/main/java/io/fabric8/jenkins/openshiftsync/BaseWatcher.java
@@ -40,10 +40,7 @@ public abstract class BaseWatcher<T> implements Watcher<T> {
     @SuppressFBWarnings("EI_EXPOSE_REP2")
     public BaseWatcher(String namespace) {
         this.namespace = namespace;
-        // this.watches = new ConcurrentHashMap<>();
     }
-
-//    public abstract Runnable getStartTimerTask();
 
     public abstract int getListIntervalInSeconds();
 
@@ -80,25 +77,11 @@ public abstract class BaseWatcher<T> implements Watcher<T> {
         };
     }
 
-//    public abstract <T> void eventReceived(io.fabric8.kubernetes.client.Watcher.Action action, T resource);
-//    public abstract <T> void eventReceived(io.fabric8.kubernetes.client.Watcher.Action action, T resource);
-
     public void startAfterOnClose(String namespace) {
         synchronized (this.lock) {
             start();
         }
     }
-
-//    @Override
-//    public void onClose(WatcherException cause) {
-//        Watcher<T> watcher = this;
-//        String namespace = getNamespace();
-//        LOGGER.info("Closing watcher: cause: " + cause + ", watcher: " + watcher);
-//        // TODO implement here what should be done when closing this watcher
-//        // TODO Let's reimplement it, using Observer pattern and notifying the
-//        // GlobalPluginConfiguration listener
-//        // super.onClose(cause);
-//    }
 
     @Override
     public void onClose() {
@@ -113,17 +96,6 @@ public abstract class BaseWatcher<T> implements Watcher<T> {
         // watcher.onClose(cause);
     }
 
-    // public synchronized void start() {
-    // lets do this in a background thread to avoid errors like:
-    // Tried proxying
-    // io.fabric8.jenkins.openshiftsync.GlobalPluginConfiguration to support
-    // a circular dependency, but it is not an interface.
-    // Runnable task = getStartTimerTask();
-    // still do the first run 100 milliseconds in
-    // this.relister = Timer.get().scheduleAtFixedRate(task, 100,
-    // getListIntervalInSeconds() * 1000, MILLISECONDS);
-    // }
-
     public void stop() {
         if (this.watch != null) {
             synchronized (this.lock) {
@@ -134,43 +106,7 @@ public abstract class BaseWatcher<T> implements Watcher<T> {
                 }
             }
         }
-        /*
-         * if (relister != null && !relister.isDone()) { relister.cancel(true); relister
-         * = null; }
-         * 
-         * for (Map.Entry<String, Watch> entry : watches.entrySet()) {
-         * entry.getValue().close(); watches.remove(entry.getKey()); }
-         */
     }
-
-//    public void stop(String namespace) {
-//        Watch watch = watches.get(namespace);
-//        if (watch != null) {
-//            watch.close();
-//            watches.remove(namespace);
-//        }
-//    }
-    // @Override
-//    public void onClose(WatcherException e, String namespace) {
-//        // scans of fabric client confirm this call be called with null
-//        // we do not want to totally ignore this, as the closing of the
-//        // watch can effect responsiveness
-//        LOGGER.info("Watch for type " + this.getClass().getName() + " closed for namespace : " + namespace);
-//        if (e != null) {
-//            synchronized (this.lock) {
-//                LOGGER.severe("Exception while watching namespace: " + namespace + ", " + e.toString());
-//                // stop(namespace);
-//                // startAfterOnClose(namespace);
-//            }
-//        }
-//    }
-
-//    public void addWatch(String key, Watch desiredWatch) {
-//        Watch watch = watches.putIfAbsent(key, desiredWatch);
-//        if (watch != null) {
-//            watch.close();
-//        }
-//    }
 
     public String toString() {
         return ReflectionToStringBuilder.toString(this, DEFAULT_STYLE, false, false)
